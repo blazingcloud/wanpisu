84d4d448c2ba8a5ae1a2435debffdfb24157416d
Judy Tuan <judytuna@gmail.com>
2012-02-16 11:12:31 -0800

    starting wanpisu

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wanpisu::Application.config.secret_token = '94ede7974a7ee893824fdcd08478271e2d379c958a9325153f0c644d4a420639c7a1d582524bd115a4bc33b52377580002f1e18051af2ad3a46cbaea35a56a74'
