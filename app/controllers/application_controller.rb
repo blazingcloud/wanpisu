6a491520ce422ad8ccc2f5b2b2331f0ddaba0ca0
Judy Tuan <judytuna@gmail.com>
2012-02-17 23:38:12 -0800

    fixed require_password to check for the correct room (instead of just being logged in at all)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   def require_password
-    unless session[:room_id]
+    unless session[:room_id] == current_room.id
       redirect_to "/session/new"
     end
   end
   
   def current_room
-    @current_room ||= Room.find(session[:room_id])
+    @current_room ||= Room.find(params[:id])
   end
 end
